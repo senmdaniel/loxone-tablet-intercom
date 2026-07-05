189fc2d4a3e6649b8efad3f8bc4e13ff093625a6
senmdaniel <senmdaniel@hotmail.com>
2026-07-05 05:58:13 +0200

    Update run.sh

--- loxone-intercom/run.sh
+++ loxone-intercom/run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 echo "🚀 Loxone Tablet Intercom Add-on wordt gestart..."
 
+# Maak de ontbrekende asterisk.conf aan als deze niet bestaat
+if [ ! -f /etc/asterisk/asterisk.conf ]; then
+    touch /etc/asterisk/asterisk.conf
+fi
+
 # Start Asterisk op de achtergrond
-asterisk -g
+asterisk -f &
+
+# Geef Asterisk 2 seconden om op te starten
+sleep 2
 
 # Start de Python API op de voorgrond
 python3 /app.py
